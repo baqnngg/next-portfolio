a26fe707ab912fe2341e06a193821ecd6a7ac479
baqnngg <il8696546@gmail.com>
2024-09-13 13:50:49 +0900

    9-13

--- pages/project.js
+++ pages/project.js
@@ -4,8 +4,8 @@ import Head from "next/head";
 import ProjectItem from "@/components/projects/project_item";
 import Grid from "@mui/material/Grid2";
 
-export default function Project({Projects}){
-    return(
+export default function Project({ Projects }) {
+    return (
         <>
             <Layout>    
                 <Head>
@@ -18,32 +18,41 @@ export default function Project({Projects}){
                 <h3>총 프로젝트 : {Projects.results.length}</h3>
                 <Grid container justifyContent="center" spacing={10}>
                     {Projects && Projects.results.map((aProject) => (
-                        <ProjectItem key={aProject.id} data={aProject}/>
-                    ))};
+                        <ProjectItem key={aProject.id} data={aProject} />
+                    ))}
                 </Grid>
             </Layout>
         </>
-    )
+    );
 }
-export async function getStaticProps(){
+
+export async function getStaticProps() {
     const option = {
-        method : 'POST',
-        headers : {
-            Accept : 'application/json',
-            'Notion-Version' : '2022-06-28',
-            'Content-Type' : 'application/json',
-            Authorization : `Bearer ${TOKEN}`
-        },body : JSON.stringify({page_size : 100})
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Notion-Version': '2022-06-28',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`
+        },
+        body: JSON.stringify({ page_size: 100 })
     };
-    const res = await fetch (`https://api.notion.com/v1/databases/${DATABASE_ID}/query`,option)
-    const Projects = await res.json()
+    const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, option);
+    const Projects = await res.json();
 
-    // const ProjectNames = Projects.result.map((aProject) => {
-    //     aProject.Properties.Name.title[0].plain_text
-    // })
-    // console.log(`projectName : ${ProjectNames}`)
+    Projects.results.sort((a, b) => {
+        const titleA = a.properties.이름.title[0]?.plain_text || '';
+        const titleB = b.properties.이름.title[0]?.plain_text || '';
 
-    return{
-        props : {Projects}
-    }
+        const isEnglishA = /^[A-Za-z]/.test(titleA);
+        const isEnglishB = /^[A-Za-z]/.test(titleB);
+
+        if (isEnglishA && !isEnglishB) return -1;
+        if (!isEnglishA && isEnglishB) return 1;
+        return titleA.localeCompare(titleB);
+    });
+
+    return {
+        props: { Projects }
+    };
 }
